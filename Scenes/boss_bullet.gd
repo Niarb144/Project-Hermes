ede4d93a693f264ce638e2105285aa4b30d6f713
Teddy <teddybrian543@gmail.com>
2026-01-21 14:15:07 +0300

    Fix asteroid bug

--- Scenes/boss_bullet.gd
+++ Scenes/boss_bullet.gd
@@ -3,14 +3,17 @@ extends Area2D
 @export var speed := 500.0
 @export var damage := 2
 
-var screen_size: Vector2
-
 func _ready():
-	screen_size = get_viewport_rect().size
 	add_to_group("enemy_bullets")
 
 func _process(delta):
 	position.y += speed * delta
 
-	if position.y > screen_size.y + 20:
+	if position.y > get_viewport_rect().size.y + 20:
+		queue_free()
+
+func _on_body_entered(body: Node):
+	if body.is_in_group("player"):
+		body.take_damage(damage)
+		print("Enemy bullet hit:", body.name)
 		queue_free()
